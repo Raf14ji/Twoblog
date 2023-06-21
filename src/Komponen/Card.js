32f79df5cd9e2630d30385bc9e8057bd16e09c8e
Raf14ji <129191985+Raf14ji@users.noreply.github.com>
2023-06-22 03:23:18 +0700

    Update Card.js

--- src/Komponen/Card.js
+++ src/Komponen/Card.js
@@ -1,24 +1,32 @@
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import bookmark from '../bookmark.svg'
 import love from '../love.svg'
 
 export const KomponenCard = (props) => {
-    return (
-        <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={props.image} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="dark" className=''>More</Button>
-                    <Button variant=''><img src={bookmark} alt="logo" /></Button>
-                    <Button variant=''><img src={love} alt="logo" /></Button>
-                </Card.Body>
-            </Card>
-        </>
-    );
-}
+        return (
+            <>
+                <div className='card p-2' style={{ width: '18rem' }}>
+                    <img className='card-img-top' src={props.image} alt='..' />
+                    <div className='card-body'>
+                        <h5 className='card-title'>{props.title}</h5>
+                        <hr className='p-0 m-0' />
+                        <p>{props.categoryId}</p>
+                        <p className='card-content'>
+                            {props.content}
+                        </p>
+                        <Button variant="dark" className=''>More</Button>
+                        <Button variant=''><img src={bookmark} alt="logo" /></Button>
+                        <Button variant=''><img src={love} alt="logo" /></Button>
+                        <div className='post-user-info row'>
+                            <img className='avatar-user' src={props.imgProfile}></img>
+                            <div className='post-user-info-content'>
+                                <p className='postBy text-sm mt-2 mb-0 p-0'> Posted by {props.username} </p>
+                                <p className='createTime text-sm m-0 p-0'>Created at</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
